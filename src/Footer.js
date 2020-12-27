85ea10d8d9f3ea2fc399a307dcc129d0e6ef3930
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2020-12-27 22:27:51 +0000

    Netlify link removed

--- src/Footer.js
+++ src/Footer.js
@@ -14,9 +14,7 @@ export default function Footer() {
             rel="noreferrer"
           >
             {" "}
-            open-sourced on GitHub</a> and hosted on <a href="https://dreamy-bhabha-3a4fed.netlify.app/"
-            target="_blank"
-            rel="noreferrer">Netlify</a>.
+            open-sourced on GitHub</a> and hosted on Netlify.
         </small>
       </footer>
     </div>
